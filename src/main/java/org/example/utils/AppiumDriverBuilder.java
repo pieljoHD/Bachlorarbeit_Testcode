a201d19a33b39ae6a6be3b51893f5d2eb4157802
johannespielmeier <johannes.pielmeier@check24.de>
2025-01-21 10:15:31 +0100

    final Testresults

--- src/main/java/org/example/utils/AppiumDriverBuilder.java
+++ src/main/java/org/example/utils/AppiumDriverBuilder.java
@@ -16,9 +16,9 @@ public class AppiumDriverBuilder {
     private URL getHubUrl() {
         URL remoteAddress = null;
         try {
-            remoteAddress = new URL("http://127.0.0.1:4723/wd/hub");
+            remoteAddress = new URL("http://127.0.0.1:4723");
         } catch (MalformedURLException e) {
-            Assert.fail("Selenium Grid address is malformed. Exception message: ", e);
+            Assert.fail("Exception message: ", e);
         }
         return remoteAddress;
     }
@@ -47,14 +47,14 @@ public class AppiumDriverBuilder {
     private DesiredCapabilities getIOSDefaultCapabilities(Simulator simulator) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "ios");
+        capabilities.setCapability("appium:platformVersion", "17.5");
         capabilities.setCapability("appium:automationName", "xcuitest");
         capabilities.setCapability("appium:appPushTimeout", 120000);
         capabilities.setCapability("appium:clearSystemFiles", true);
         capabilities.setCapability("appium:maxTypingFrequency", 30);
         capabilities.setCapability("appium:udid", simulator.uuid);
         capabilities.setCapability("appium:wdaLocalPort", simulator.wdaLocalPort);
-        capabilities.setCapability("appium:derivedDataPath","/Users/johannespielmeier/Library/Developer/Xcode/DerivedData");
-        //let appium choose device
+        capabilities.setCapability("appium:derivedDataPath","/Users/johannespielmeier/Library/Developer/Xcode/DerivedData/"+simulator.uuid);
 
         return capabilities;
     }
